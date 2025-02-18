4d7076c0ca2e2dc6b67188d244a3569428368f0a
NTnik <kolyan1038@gmail.com>
2025-02-18 16:50:00 +0300

    Add-Exception

--- src/main/java/org/example/service/SleepService.java
+++ src/main/java/org/example/service/SleepService.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import java.util.List;
+import org.example.exception.AdviceNotFoundException;
 import org.example.model.SleepAdvice;
 import org.example.repository.SleepRepository;
 import org.springframework.stereotype.Service;
@@ -37,7 +38,11 @@ public class SleepService {
      * @return объект SleepAdvice
      */
     public SleepAdvice getAdviceById(int id) {
-        return sleepRepository.getAdviceById(id);
+        List<SleepAdvice> advices = sleepRepository.getAllAdvices();
+        if (id < 0 || id >= advices.size()) {
+            throw new AdviceNotFoundException("Совет с ID " + id + " не найден.");
+        }
+        return advices.get(id);
     }
     
     /**
@@ -48,6 +53,18 @@ public class SleepService {
      * @return список отфильтрованных советов
      */
     public List<SleepAdvice> getFilteredAdvices(Integer minHours, Integer maxHours) {
-        return sleepRepository.getFilteredAdvices(minHours, maxHours);
+        if (minHours != null && (minHours < 1 || minHours > 24)
+                || maxHours != null && (maxHours < 1 || maxHours > 24)) {
+            throw new AdviceNotFoundException("Неверное кол-во сна.");
+        }
+
+        List<SleepAdvice> filteredAdvices = sleepRepository.getFilteredAdvices(minHours, maxHours);
+
+        if (filteredAdvices.isEmpty()) {
+            throw new AdviceNotFoundException("Советы по заданным параметрам не найдены.");
+        }
+
+        return filteredAdvices;
     }
+
 }
